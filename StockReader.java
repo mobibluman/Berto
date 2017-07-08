f657d0e1b4b36a20ebcb9f0f7aeb940c895f7b39
mobibluman <mobibluman@yahoo.com>
2017-07-08 15:34:50 -0400

    Add files via upload

--- StockReader.java
+++ StockReader.java
@@ -1,50 +1,43 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StockReader{
 
 	public static void main(String[] args){
 		String filename = "NASDAQ_AAPL.txt";          	//name of the file
-		Stock[] AAPL = initializeStocks(filename);		//initialization of stock array for company
-//		System.out.println("dank" + AAPL.length);
-//		System.out.println(AAPL[2012]);
+		List<Stock> AAPL = initializeStocks(filename);		//initialization of stock array for company
+		System.out.println(AAPL.get(14).toString()); 		//Example to access 15th element of the list
 		
 	}
 	
-	public static Stock[] initializeStocks(String filename){ //returns an array of stocks read from the file
+	public static List<Stock> initializeStocks(String filename){ //returns an array of stocks read from the file
 		BufferedReader bReader = null;
 		FileReader fReader = null;
 		
 		try {		// try catch for the buffered reader
 
 			bReader = new BufferedReader(new FileReader(filename));
-			int count = 0;
 			String line;
 			String tokens[];
-			bReader.readLine();
+			bReader.readLine(); //remove header line
+			List<Stock> temps = new ArrayList<Stock>();  //list to hold each stock
+			
 			while ((line = bReader.readLine()) != null){ //opening a buffered reader to count the number of valid lines
 				tokens = line.split(",");
-				if(tokens.length == 7){
-					count++;
-				}
+				if(tokens.length == 7){  //checking the line is valid
+					temps.add(new Stock(tokens[0], Long.parseLong(tokens[1]), Double.parseDouble(tokens[2]), Double.parseDouble(tokens[3]), Double.parseDouble(tokens[4]), Double.parseDouble(tokens[5]), Integer.parseInt(tokens[6])));
+				}						// adding to the list
 			}
 			bReader.close();
 			
-			Stock[] stocks = new Stock[count];
-			bReader = new BufferedReader(new FileReader(filename)); //second buffered reader in order to read and initialize the stock array
-			bReader.readLine();
-			count = 0;
-			while ((line = bReader.readLine()) != null){
-				tokens = line.split(",");							//tokenizing the string
-				//<ticker>,<date>,<open>,<high>,<low>,<close>,<vol> -> constructor format
-				if(tokens.length == 7){
-					stocks[count] = new Stock(tokens[0], Long.parseLong(tokens[1]), Double.parseDouble(tokens[2]), Double.parseDouble(tokens[3]), Double.parseDouble(tokens[4]), Double.parseDouble(tokens[5]), Integer.parseInt(tokens[6]));
-					System.out.println(stocks[count].toString() + "\n" + count + "\n");
-					count++;
-				}
+			for(Stock s : temps){ 					//printing out each stock for checking
+				System.out.println(s.toString());
 			}
-			return stocks;
+			
+			return temps;
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			System.out.println("Exception");
